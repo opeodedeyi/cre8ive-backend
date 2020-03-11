717969b96c84fc54eff85d60e0d54c96415884c3
opeodedeyi <opeyemiodedeyi@gmail.com>
2020-03-11 03:26:54 +0100

    Update settings.py

--- creativeAppApi/settings.py
+++ creativeAppApi/settings.py
@@ -105,7 +105,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1,
+    'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
